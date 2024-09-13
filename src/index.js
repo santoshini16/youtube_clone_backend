078a91a1de21e55ba1de24a389d10953525d7341
santoshini <santoshinipradhan01@gmail.com>
2024-09-13 07:44:32 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const express = require('express')
+
 const mongoConn= require('./db/index')
 const dotenv= require('dotenv').config()
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
 
 
 mongoConn()
-app.listen(port, () => {
-  console.log(` app listening on port ${port}`)
+.then(()=>{
+  app.listen(port, () => {
+    console.log(` app listening on port ${port}`)
+  })
+})
+.catch((err)=>{
+  console.log("mongo failed",err)
 })
